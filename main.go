22a5c65d2d769d70426a016e7d41bbce63fefea9
Matthieu Leboeuf <contact@matthieul.dev>
2023-02-19 12:50:55 +0100

    add 100 millisecond delay

--- main.go
+++ main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"github.com/warthog618/gpiod"
 	"github.com/warthog618/gpiod/device/rpi"
+	"time"
 )
 
 func main() {
-	l, err := gpiod.RequestLine("gpiochip0", rpi.GPIO2, gpiod.AsOutput(0))
+	l, err := gpiod.RequestLine("gpiochip0", rpi.GPIO2)
 	if err != nil {
 		panic(err)
 	}
+	err = l.SetValue(0)
+	if err != nil {
+		panic(err)
+	}
+	time.Sleep(100 * time.Millisecond)
 	err = l.SetValue(1)
+	if err != nil {
+		panic(err)
+	}
+	err = l.Close()
 	if err != nil {
 		panic(err)
 	}
