21348082c2590c0cf2b03381654ce98d1e38235f
Matthieu Leboeuf <contact@matthieul.dev>
2023-02-19 13:44:22 +0100

    added webserver

--- main.go
+++ main.go
@@ -3,14 +3,38 @@ package main
 import (
 	"github.com/warthog618/gpiod"
 	"github.com/warthog618/gpiod/device/rpi"
+	"log"
+	"net/http"
 	"time"
 )
 
-func main() {
-	c, _ := gpiod.NewChip("gpiochip0", gpiod.WithConsumer("myapp"))
-	l, _ := c.RequestLine(rpi.GPIO2)
+func up(w http.ResponseWriter, r *http.Request) {
+	l, _ := gpiod.RequestLine("gpiochip0", rpi.GPIO3, gpiod.AsOutput(1))
 	_ = l.SetValue(0)
 	time.Sleep(100 * time.Millisecond)
 	_ = l.SetValue(1)
 	_ = l.Close()
+	w.WriteHeader(http.StatusOK)
+}
+
+func down(w http.ResponseWriter, r *http.Request) {
+	l, _ := gpiod.RequestLine("gpiochip0", rpi.GPIO2, gpiod.AsOutput(1))
+	_ = l.SetValue(0)
+	time.Sleep(100 * time.Millisecond)
+	_ = l.SetValue(1)
+	_ = l.Close()
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	// Register routes
+	mux.HandleFunc("/open", up)
+	mux.HandleFunc("/close", down)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
